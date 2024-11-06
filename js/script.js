ff6adddcf13ce6319c607495e4f64ef4d987199f
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-11-07 00:02:06 +0100

    feat: updateContactList() create and show dom elements for all contacts in the array

--- js/script.js
+++ js/script.js
@@ -17,11 +17,46 @@ function addContact() {
     };
     contacts.push(contact); // Lägg till kontakten i arrayen
 
+    updateContactList();
 
-    console.log(contacts); // kontakten har laggts in i arrayen 
 
     // Töm input-fälten
     nameInput.value = '';
     phoneInput.value = '';
 }
 
+function updateContactList() {
+    const contactList = document.getElementById('contactList');
+    contactList.innerHTML = ''; // Rensa listan för fö
+
+    // Loopa igenom alla kontakter i arrayen och skapa DOM-element för varje
+    contacts.forEach((contact, index) => {
+        const listItem = document.createElement('li');
+        listItem.className = 'contact-item';
+
+        const nameField = document.createElement('input');
+        nameField.type = 'text';
+        nameField.value = contact.name;
+        nameField.disabled = true;
+
+        const phoneField = document.createElement('input');
+        phoneField.type = 'text';
+        phoneField.value = contact.phone;
+        phoneField.disabled = true;
+
+        const editButton = document.createElement('button');
+        editButton.innerText = 'Ändra';
+        editButton.onclick = () => editContact(index); // TOD skapa funktionen till edit knappen
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'Radera';
+        deleteButton.onclick = () => deleteContact(index); //TOD Skapa funktionen till knapp
+
+        // Lägg till elementen i listan
+        listItem.appendChild(nameField);
+        listItem.appendChild(phoneField);
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+        contactList.appendChild(listItem);
+    });
+}
