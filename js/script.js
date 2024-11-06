f91dc4e46c3a084eceb3cd5b4676d80f4a8a45d4
jonatangroncode <jonatan.gron.code@hotmail.com>
2024-11-06 22:06:31 +0100

    feat: changing to adding contact to an array instead of direct dom manipulating

--- js/script.js
+++ js/script.js
@@ -1,43 +1,26 @@
+const contacts = []; 
+
+
 const registerForm = document.getElementById('add-contact-form');
 registerForm.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    addContact(); 
+    event.preventDefault();
+    addContact();
 });
 
 function addContact() {
     const nameInput = document.getElementById('name');
     const phoneInput = document.getElementById('telefone');
-    const contactList = document.getElementById('contactList');
-
-    // Skapa dom nya HTML-elementen för kontakten som hamnar inne i listan
-    const listItem = document.createElement('li');
-    listItem.className = 'contact-item';
-
-    const nameField = document.createElement('input');
-    nameField.type = 'text';
-    nameField.value = nameInput.value;
-    nameField.disabled = true;
-
-    const phoneField = document.createElement('input');
-    phoneField.type = 'text';
-    phoneField.value = phoneInput.value;
-    phoneField.disabled = true;
-
-    const editButton = document.createElement('button');
-    editButton.innerText = 'Ändra';
-    editButton.onclick = () => editContact(nameField, phoneField, editButton);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Radera';
-    deleteButton.onclick = () => contactList.removeChild(listItem);
+    
+    const contact = {
+        name: nameInput.value,
+        phone: phoneInput.value
+    };
+    contacts.push(contact); // Lägg till kontakten i arrayen
 
 
-    listItem.appendChild(nameField);
-    listItem.appendChild(phoneField);
-    listItem.appendChild(editButton);
-    listItem.appendChild(deleteButton);
-    contactList.appendChild(listItem);
+    console.log(contacts); // kontakten har laggts in i arrayen 
 
+    // Töm input-fälten
     nameInput.value = '';
     phoneInput.value = '';
 }
